2c059dfdc98e45ea5fb7aa3da8bf31bdfd8229b8
Johannes Habel <EchterAlsFake@proton.me>
2025-01-06 21:00:40 +0100

    - async release 1.7.2 - fixed tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hqporner_api-async",
-    version="1.7.1",
+    version="1.7.2",
     packages=find_packages(),
     install_requires=[ "bs4", "eaf_base_api-async"],
     entry_points={
